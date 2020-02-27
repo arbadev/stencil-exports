f20bdbc39f90a2cb3487e9bb7cd2b27a9d9c7e9d
Andres Barradas <arbadev@gmail.com>
2020-02-26 20:31:34 -0500

    intial

---
+++ stencil.config.ts
@@ -0,0 +1,18 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'text-anim',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader'
+    },
+    {
+      type: 'docs-readme'
+    },
+    {
+      type: 'www',
+      serviceWorker: null // disable service workers
+    }
+  ]
+};
